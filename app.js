f39ae39dd4f62d619643c578cb2a22d22ecc19b8
pushpabrol <pushp.abrol@gmail.com>
2023-08-29 18:15:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+
+import express from "express"
+import path from "path"
+import bodyParser from "body-parser"
+import session from "express-session"
+import cookieParser from "cookie-parser"
+
+import dotenv from "dotenv"
+const __dirname = path.resolve()
+dotenv.config({ path: path.join(__dirname, './.env') })
+
+console.log(process.env);
+import onfido from "./routes/onfido.js"
+
+const cookieSecret = process.env.APP_SECRET
+
+const app = express()
+
+//app.set("trust proxy", "1")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser(cookieSecret))
+
+const memoryStore = new session.MemoryStore()
+app.use(
+  session({
+    secret: cookieSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: app.get("env") === "development" ? false : true },
+    store: memoryStore
+  })
+)
+
+app.set("views", path.join(__dirname, "/views"))
+app.set("view engine", "pug")
+app.use(express.static(path.join(__dirname, "/public")))
+
+app.use("/redirect-rule", onfido)
+
+app.use((req, res, next) => {
+  next(new Error("Not Found"))
+})
+
+if (app.get("env") === "development") {
+  app.use((err, req, res) => {
+    res.status(500).render("error", {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+app.use((err, req, res) => {
+  res.status(500).render("error", {
+    message: err.message
+  })
+})
+
+// Express requires the next function (or specific function signature) to include the 4 arguments: https://github.com/expressjs/generator/issues/78
+// as such, we are telling eslint to ignore the no-unused-vars rule for the error handler middleware
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(500).render("error", {
+    message: err.message
+  })
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on ${process.env.PORT}`)
+})
---
+++ routes/onfido.js
@@ -0,0 +1,176 @@
+import express from "express"
+import  url from "url"
+import path from "path"
+
+import { Onfido, Region } from "@onfido/api"
+import jwt from "jsonwebtoken"
+
+import dotenv from "dotenv"
+const __dirname = path.resolve()
+console.log(__dirname);
+dotenv.config({ path: path.join(__dirname, './.env') })
+
+const onfidoClient = new Onfido({
+  apiToken: process.env.ONFIDO_API_TOKEN,
+  region:
+    process.env.ONFIDO_REGION === "EU"
+      ? Region.EU
+      : process.env.ONFIDO_REGION === "US"
+      ? Region.US
+      : process.env.ONFIDO_REGION === "CA"
+      ? Region.CA
+      : Region.EU
+})
+const router = express.Router()
+
+router.get("/path/:sessionToken", (req, res) => {
+  console.log(req.params)
+  const query = url.parse(req.url, true).query
+  const sessionToken = req.params.sessionToken
+  const auth0State = String(query.state)
+  req.session.auth0State = auth0State
+  const payload = jwt.verify(sessionToken, process.env.APP_SECRET, {
+    ignoreExpiration: true
+  })
+
+  if (!payload.exp) {
+    res.status(403).render("error", {
+      message: "Session Token is expired."
+    })
+  }
+
+  req.session.auth0Payload = payload
+
+  const { applicant } = payload
+  req.session.applicant = applicant
+  return onfidoClient.sdkToken
+    .generate({
+      applicantId: applicant,
+      referrer: process.env.ONFIDO_REFERRER_PATTERN
+    })
+    .then(sdkToken => {
+      res.status(200).render("onfido", {
+        sdkToken
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).render("error", {
+        message: error
+      })
+    })
+})
+
+router.get("/", (req, res) => {
+  const query = url.parse(req.url, true).query
+  const sessionToken = String(query.session_token)
+  const auth0State = String(query.state)
+  req.session.auth0State = auth0State
+  const payload = jwt.verify(sessionToken, process.env.APP_SECRET, {
+    ignoreExpiration: true
+  })
+
+
+  if (!payload.exp) {
+    res.status(403).render("error", {
+      message: "Session Token is expired."
+    })
+  }
+
+  req.session.auth0Payload = payload
+
+  const { applicant } = payload
+  req.session.applicant = applicant
+  return onfidoClient.sdkToken
+    .generate({
+      applicantId: applicant,
+      referrer: process.env.ONFIDO_REFERRER_PATTERN
+    })
+    .then(sdkToken => {
+      res.status(200).render("onfido", {
+        sdkToken
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).render("error", {
+        message: error
+      })
+    })
+})
+
+router.post("/", (req, res) => {
+  const { auth0State, auth0Payload, checkId } = req.session
+  const complete = req.body.onfidoComplete
+  if (complete) {
+    //eslint-disable-next-line
+    //@ts-ignore
+    return onfidoClient.check
+      .find(checkId)
+      .then(response => {
+        const sessionToken = {
+          checkStatus: response.status,
+          checkResult: response.result,
+          applicant: response.applicantId,
+          ...auth0Payload,
+          state: auth0State
+        }
+        const signed = jwt.sign(sessionToken, process.env.APP_SECRET)
+        console.log(auth0Payload)
+        //eslint-disable-next-line
+        //@ts-ignore
+        const continueUrl = `${auth0Payload.iss}continue/reset-password?state=${auth0State}&session_token=${signed}`
+        //const continueUrl = `${auth0Payload.iss}continue?state=${auth0State}`
+        res.redirect(continueUrl)
+      })
+      .catch(error => {
+        res.status(500).render("error", { message: error })
+      })
+  } else {
+    return onfidoClient.check
+      .find(checkId)
+      .then(response => {
+        const sessionToken = {
+          checkStatus: response.status,
+          checkResult: response.result,
+          ...auth0Payload,
+          state: auth0State
+        }
+        const signed = jwt.sign(sessionToken, process.env.APP_SECRET)
+        console.log(auth0Payload)
+        const continueUrl = `${auth0Payload.iss}continue/reset-password?state=${auth0State}&session_token=${signed}`
+        res.redirect(continueUrl)
+      })
+      .catch(error => {
+        res.status(500).render("error", { message: error })
+      })
+  }
+})
+
+router.get("/check", (req, res) => {
+  const { applicant } = req.session
+  const reportNames = process.env.ONFIDO_REPORT_NAMES.split(",")
+  return onfidoClient.check
+    .create({ applicantId: applicant, reportNames })
+    .then(response => {
+      req.session.checkId = response.id
+      res.status(200).json({ status: response.status })
+    })
+    .catch(error => {
+      res.status(500).json({ message: error })
+    })
+})
+
+router.get("/status", (req, res) => {
+  const { checkId } = req.session
+  return onfidoClient.check
+    .find(checkId)
+    .then(response => {
+      res.status(200).json({ status: response.status })
+    })
+    .catch(error => {
+      res.status(500).json({ message: error })
+    })
+})
+
+export default router
